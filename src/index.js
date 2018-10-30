cef86e6c696f24d378671962679c9c9402ddee7d
Julie Park <jspark721@gmail.com>
2018-10-30 16:28:20 -0700

    add style file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/style.css'; //webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/boostrap.min.css';
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+)
